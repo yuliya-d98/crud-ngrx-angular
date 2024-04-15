e36a6601b7ad50400cc7b47ec131bab4696cbce0
Yuliya Dubovik <feelgloom@gmail.com>
2024-04-15 23:33:33 +0300

    feat: use NGRX store in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {BookStoreModule} from "../store/book/book-store.module";
+import * as fromBooks from '../store/book/index';
+import {IBook} from "../interfaces/book.interface";
+import {Observable} from "rxjs";
+import {select, Store} from "@ngrx/store";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -15,6 +20,41 @@ import {BookStoreModule} from "../store/book/book-store.module";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'crud-ngrx-angular';
+export class AppComponent implements OnInit {
+  books$: Observable<IBook[]>;
+  isLoading$: Observable<boolean>;
+
+  constructor(private readonly store: Store) {
+  }
+
+  ngOnInit(): void {
+    this.initDispatch();
+    this.initSubscriptions();
+  }
+
+  onCreateBook(name: string): void {
+    this.store.dispatch(fromBooks.createBook({
+      book: {
+        id: Math.random(),
+        name
+      }
+    }));
+  }
+
+  onUpdateBook(book: IBook): void {
+    this.store.dispatch(fromBooks.updateBook({book}));
+  }
+
+  onDeleteBook(book: IBook): void {
+    this.store.dispatch(fromBooks.deleteBook({book}));
+  }
+
+  private initDispatch(): void {
+    this.store.dispatch(fromBooks.getBooks());
+  }
+
+  private initSubscriptions(): void {
+    this.books$ = this.store.pipe(select(fromBooks.selectBooksList));
+    this.isLoading$ = this.store.pipe(select(fromBooks.selectBookIsLoading));
+  }
 }
